b424109d540031081c0d0b383524895ecc379005
Gabriel <gabriel.tomitsuka@montreus.com>
2015-03-14 23:13:28 +0100

    Oops

--- js/docs.js
+++ js/docs.js
@@ -1,12 +1,8 @@
-<script>
-  YUI().use('node-base', 'node-event-delegate', function (Y) {
-
+YUI().use('node-base', 'node-event-delegate', function (Y) {
     var menuButton = Y.one('.nav-menu-button');
     var nav = Y.one('#nav');
-
     // Setting the active class name expands the menu vertically on small screens.
     menuButton.on('click', function (e) {
       nav.toggleClass('active');
-    });
   });
-</script>
+});
